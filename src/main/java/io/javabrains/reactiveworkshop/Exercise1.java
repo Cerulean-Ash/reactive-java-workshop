17c07bc91756f2fafc73d574cc097b4427e9c378
Ashley Lampard <ashley.lampard@ocado.com>
2025-02-27 10:55:27 +0000

    backup

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,30 +1,42 @@
 package io.javabrains.reactiveworkshop;
 
 public class Exercise1 {
-
-    public static void main(String[] args) {
-
-        // Use StreamSources.intNumbersStream() and StreamSources.userStream()
-
-        // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
-
-        // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
-
-        // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
-
-        //  Print the first number in intNumbersStream that's greater than 5.
-        //  If nothing is found, print -1
-        // TODO: Write code here
-
-        // Print first names of all users in userStream
-        // TODO: Write code here
-
-        // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
-
-    }
-
+  
+  public static void main(String[] args) {
+    
+    // Use StreamSources.intNumbersStream() and StreamSources.userStream()
+    
+    // Print all numbers in the intNumbersStream stream
+    // TODO: Write code here
+    StreamSources.intNumbersStream().forEach(System.out::println);
+    
+    // Print numbers from intNumbersStream that are less than 5
+    // TODO: Write code here
+    StreamSources.intNumbersStream().filter(i -> i < 5).forEach(System.out::println);
+    
+    // Print the second and third numbers in intNumbersStream that's greater than 5
+    // TODO: Write code here
+    StreamSources.intNumbersStream().filter(i -> i > 5).skip(1).limit(2).forEach(System.out::println);
+    
+    //  Print the first number in intNumbersStream that's greater than 5.
+    //  If nothing is found, print -1
+    // TODO: Write code here
+    StreamSources.intNumbersStream().filter(i -> i > 5).findFirst().ifPresentOrElse(System.out::println, () -> System.out.println(-1));
+    
+    // Print first names of all users in userStream
+    // TODO: Write code here
+    StreamSources.userStream().map(user -> user.getFirstName()).forEach(System.out::println);
+    
+    // Print first names in userStream for users that have IDs from number stream
+    // TODO: Write code here
+    StreamSources.userStream().filter(user -> StreamSources.intNumbersStream().anyMatch(i -> i == user.getId()))
+        .map(user -> user.getFirstName())
+        .forEach(System.out::println);
+    // or
+    StreamSources.intNumbersStream().flatMap(id -> StreamSources.userStream()
+            .filter(user -> user.getId() == id)).map(user -> user.getFirstName())
+        .forEach(System.out::println);
+    
+  }
+  
 }
--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -3,19 +3,21 @@ package io.javabrains.reactiveworkshop;
 import java.io.IOException;
 
 public class Exercise2 {
-
-    public static void main(String[] args) throws IOException {
-
-        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
-
-        // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
-
-        // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
-
-        System.out.println("Press a key to end");
-        System.in.read();
-    }
-
+  
+  public static void main(String[] args) throws IOException {
+    
+    // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
+    
+    // Print all numbers in the ReactiveSources.intNumbersFlux stream
+    // TODO: Write code here
+    ReactiveSources.intNumbersFlux().subscribe(e -> System.out.println(e));
+    
+    // Print all users in the ReactiveSources.userFlux stream
+    // TODO: Write code here
+    ReactiveSources.userFlux().subscribe(e -> System.out.println(e));
+    
+    System.out.println("Press a key to end");
+    System.in.read();
+  }
+  
 }
